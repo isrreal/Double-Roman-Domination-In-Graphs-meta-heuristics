40fc80727e95d71f9b8cf37bf9efae68b307fcf9
leggen-assis <souzaferreira437@gmail.com>
2024-10-09 17:21:00 -0300

    Graph atribute added

--- src/Cromossomo.cpp
+++ src/Cromossomo.cpp
@@ -2,28 +2,33 @@
 
 Cromossomo::~Cromossomo() {}
 
-Cromossomo::Cromossomo(std::vector<int> genes) {
+Cromossomo::Cromossomo(std::vector<int> genes, Graph* graph) {
 	this->quantidadeDeGenes = genes.size();
 	this->genes = genes;
+	this->graph = graph;
 }
 
-Cromossomo::Cromossomo(size_t quantidadeDeGenes) {
+Cromossomo::Cromossomo(size_t quantidadeDeGenes, Graph* graph) {
     this->quantidadeDeGenes = quantidadeDeGenes;
     this->genes = std::vector<int>(quantidadeDeGenes, -1);
+    this->graph = graph;
 } 
 
-Cromossomo::Cromossomo(std::vector<int> primeiraMetade, std::vector<int> segundaMetade) {
+Cromossomo::Cromossomo(std::vector<int> primeiraMetade, std::vector<int> segundaMetade, Graph* graph) {
     this->quantidadeDeGenes = primeiraMetade.size() + segundaMetade.size(); 
     this->genes = primeiraMetade;
     this->genes.insert(this->genes.end(), segundaMetade.begin(), segundaMetade.end());
+    this->graph = nullptr;	
 }
 
-std::ostream& operator<<(std::ostream& os, Cromossomo* cromossomo) {
-    for (size_t i = 0; i < cromossomo->quantidadeDeGenes; ++i) 
-        std::cout << cromossomo->genes[i] << " ";
+Cromossomo::Cromossomo(Cromossomo* cromossomo) {
+    this->quantidadeDeGenes = cromossomo->quantidadeDeGenes;
+    this->genes = cromossomo->genes;
+    this->graph = cromossomo->graph;
+}
+
+std::ostream& operator<<(std::ostream& os, const Cromossomo& cromossomo) {
+    for (const auto& it: cromossomo.genes)
+        os << it << " ";
     return os;
 }
-
-
-
-

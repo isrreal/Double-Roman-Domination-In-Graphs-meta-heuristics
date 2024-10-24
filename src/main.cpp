39fa3d353d6041f45bd11bc08e5d086681e29c52
leggen-assis <souzaferreira437@gmail.com>
2024-10-24 10:31:17 -0300

    terminal input mechanism added

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,17 @@
 #include "Graph.hpp"             
 #include "AntColonyOptimization.hpp"
 
-int main() {
-   // order, isDirected, edgeProbability   
-    Graph graph(100, false, 0.1);
-    // graph, populationSize, genesSize, generations, numberOfAnts, iterations
-   	DoubleRomanDomination* drd = new DoubleRomanDomination(graph, 100, graph.getOrder(), 100, 20, 10); 
-    std::cout << "Double Roman Domination Number computed by Genetic Algorithm: " << drd->getGamma2rGeneticAlgorithm() << std::endl;
-    std::cout << "Double Roman Domination Number computed by ACO: " << drd->getGamma2rACO() << std::endl;
-
-    return 0;}
+int main(int argc, char** argv) {
+    if (argc == 3) {
+       // order, isDirected, edgeProbability   
+        Graph graph(std::stoi(argv[1]), false, std::stof(argv[2]));
+        // graph, populationSize, genesSize, generations, numberOfAnts, iterations
+        DoubleRomanDomination* drd = new DoubleRomanDomination(graph, 100, graph.getOrder(), 100, 20, 10); 
+        std::cout << "Double Roman Domination Number computed by Genetic Algorithm: " << drd->getGamma2rGeneticAlgorithm() << std::endl;
+        std::cout << "Double Roman Domination Number computed by ACO: " << drd->getGamma2rACO() << std::endl;
+        delete drd;
+        return 0;
+    }
+    else
+        return -1;
+}

ab2804d867c18dee0184e1f56f80906e94849637
leggen-assis <souzaferreira437@gmail.com>
2024-10-29 15:51:51 -0300

    argv added for populationSize, generations, numberOfAnts, iterations

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,15 @@
 #include "AntColonyOptimization.hpp"
 
 
-int main() {
-	Graph graph("graph.txt", false);
-    // graph, populationSize, genesSize, generations, numberOfAnts, iterations
-    DoubleRomanDomination* drd = new DoubleRomanDomination(graph, 1000, graph.getOrder(), 100, 20, 10); 
-    std::cout << "Double Roman Domination Number computed by Genetic Algorithm: " << drd->getGamma2rGeneticAlgorithm() << std::endl;
-    std::cout << "Double Roman Domination Number computed by ACO: " << drd->getGamma2rACO() << std::endl;
-    delete drd;
+int main(int argc, char** argv) {
+    if (argc > 4) {
+        Graph graph("graph.txt", false);
+        // graph, populationSize, genesSize, generations, numberOfAnts, iterations
+        DoubleRomanDomination* drd = new DoubleRomanDomination(graph, std::stoi(argv[1]), graph.getOrder(), std::stoi(argv[2]), std::stoi(argv[3]), std::stoi(argv[4])); 
+        std::cout << "Double Roman Domination Number computed by Genetic Algorithm: " << drd->getGamma2rGeneticAlgorithm() << std::endl;
+        std::cout << "Double Roman Domination Number computed by ACO: " << drd->getGamma2rACO() << std::endl;
+        delete drd;
+    }
+
 	return 0;
 }
